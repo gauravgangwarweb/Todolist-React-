7bb9a70c3d3d23941d3fdfac5baaf11842b0303d
gauravgangwarweb <gauravgangwar501@gmail.com>
2023-01-16 21:45:20 +0530

    Add main todocontainer code

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,12 +1,13 @@
-import React from "react"
-class TodoContainer extends React.Component {
+import React, { Component } from 'react';
+// eslint-disable-next-line
+class TodoContainer extends Component {
   render() {
     return (
       <div>
         <h1>Hello from Create React App</h1>
         <p>I am in a React Component!</p>
       </div>
-    )
+    );
   }
 }
-export default TodoContainer
+export default TodoContainer;
