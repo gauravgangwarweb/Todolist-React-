ea86b39e0c1b2ebbe915a5274c08ed6905ba0d03
gauravgangwarweb <gauravgangwar501@gmail.com>
2023-01-18 15:55:02 +0530

    Add main js file

--- src/index.js
+++ src/index.js
@@ -4,4 +4,9 @@ import ReactDOM from 'react-dom';
 // component file
 import TodoContainer from './components/TodoContainer';
 
-ReactDOM.render(<TodoContainer />, document.getElementById('root'));
+// stylesheet
+import './App.css';
+
+ReactDOM.render(<React.StrictMode>
+  <TodoContainer />
+                </React.StrictMode>, document.getElementById('root'));
