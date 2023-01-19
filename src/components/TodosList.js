2c19b537bc39cb2de053b13b56d78c45a27e7e00
gauravgangwarweb <gauravgangwar501@gmail.com>
2023-01-19 14:07:03 +0530

    Add Todo list file with hooks

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,14 +1,19 @@
-import React, { Component } from 'react';
-import TodoItem from './TodoItem';
+import React from "react"
+import TodoItem from "./TodoItem"
 
-export class TodosList extends Component {
-  render() {
-    return (
-      <ul>
-        {this.props.todos.map((todo) => <TodoItem key={todo.id} todo={todo} handleChangeProps={this.props.handleChangeProps} deltodoProps={this.props.deltodoProps} setUpdate={this.props.setUpdate} />)}
-      </ul>
-    );
-  }
+const TodosList = props => {
+  return (
+    <ul>
+      {props.todos.map(todo => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          handleChangeProps={props.handleChangeProps}
+          deleteTodoProps={props.deleteTodoProps}
+          setUpdate={props.setUpdate}
+        />
+      ))}
+    </ul>
+  )
 }
-
-export default TodosList;
+export default TodosList
